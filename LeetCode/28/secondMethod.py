56285f85def99a544b86de397dfe2173cf516d41
llfccc <llfccc@live.cn>
2020-03-28 00:46:28 +0800

    add

--- LeetCode/28/secondMethod.py
+++ LeetCode/28/secondMethod.py
@@ -1,5 +1,7 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        import pdb;pdb.set_trace()
+
         if not needle:
             return 0
         T=haystack
@@ -9,54 +11,47 @@ class Solution:
         len_P=len(P)
 
         next_list=self.get_next_val(P)
-        print(next_list)
-        return 
         if len_P>len_T:
             return -1
         i=0
         j=0
-        n=0
-        
-        while(i<len_T):
-            # print("第{n}轮---------------".format(n=n))
-            n+=1
-            while(j<=len_P):
-                if i>=len_T:
-                    break
-                # print("###",i,j)
-                # print("***",T[i],P[j])
-                if T[i]==P[j]:
+
+        while(i<len_T and j<len_P) :
+                if T[i]==P[j]:  # 如果符合，则对比下一个字符
+                    j+=1
+                    i+=1
+                elif j==-1: # 如果跳转到了P的首个字符，则让对比下一个字符
                     j+=1
                     i+=1
                 else:
-                    j=next_list[j]
-                    i=i                    
-                    break
+                    j=next_list[j]  # 如果对比不符合，则跳转到next数组对应的p去
+                    i=i  
+
                 if (j==len_P):
                     return i-j               
                  
         return -1
+
     def get_next_val(self,p):
-        next_list=[-10]*len(p)
+        next_list=[-10]*(len(p)+1)
         next_list[0] = -1
         j = 0
         k = -1
-        while j < len(p) - 1:
-            print("*****",j,k)
-            print("#####",p[j],p[k])
+        while j < len(p)-1 :
             if k == -1 or p[j] == p[k]:
-                j = j + 1
-                k = k + 1
+                j  += 1
+                k  += 1
                 next_list[j] = k
             else:
-                k = next_list[k]      
+                k = next_list[k]      #整个函数最关键的一句，用来依靠前缀和后缀相同的特性来跳过一些情况减少复杂度
         return next_list
 
 
 
 
 
-haystack="hello24234234df"
-needle="ababababe"
+haystack="mississippi"
+
+needle="a"
 result=Solution().strStr(haystack,needle)
 print(result)
